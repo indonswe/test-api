35bd4a9f3029897b3507575201a71bb20f06c313
Mehrdad Javan <mehrdad.javan@adapta.se>
2021-11-12 17:36:12 +0100

    clean code

--- src/main/java/se/lexicon/api/entity/Person.java
+++ src/main/java/se/lexicon/api/entity/Person.java
@@ -40,43 +40,5 @@ public class Person {
         this.title = title;
     }
 
-    public Integer getId() {
-        return id;
-    }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
 }
